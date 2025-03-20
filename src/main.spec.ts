3de5e89361ea693d39478956f2334f408546415e
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2025-03-20 15:47:33 -0400

    Add vite-tsconfig-paths

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from "@effect/vitest"
 import { Effect } from "effect"
 
+import { env } from "~/env"
+
 test("minimal", () =>
   Effect.gen(function* () {
-    const value = yield* Effect.succeed(1)
-    expect(value).toBe(1)
+    const value = yield* Effect.succeed(env.MY_VARIABLE)
+    expect(value).toBe(env.MY_VARIABLE)
   }))
